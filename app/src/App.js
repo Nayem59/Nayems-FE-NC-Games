7c470fc4e686a69669b96fe036148de39c2191d1
Nayem <nayem.alam97@gmail.com>
2023-03-13 12:23:16 +0000

    added Header component

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/reviews" element={<Reviews />} />
+        {/* prep for routes down the line */}
+        {/* <Route path="/reviews/:review_id" element={<SingleReview />} /> */}
+        {/* <Route path="/categories" element={<Categories />} /> */}
+        {/* <Route path="/categories/:category" element={<SingleCategory />} /> */}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
