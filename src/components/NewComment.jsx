db68cbfdaeaf13843f2f3475d7255ed77316f475
Nayem <nayem.alam97@gmail.com>
2023-03-14 18:59:51 +0000

    fixed pagination and basic controlled component

--- src/components/NewComment.jsx
+++ src/components/NewComment.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 export const NewComment = () => {
+  const [text, setText] = useState("");
+  const [newComment, setNewComment] = useState("");
+
+  const handleText = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleAddBtn = () => {
+    setNewComment(text);
+    setText("");
+  };
+
+  console.log(newComment);
+
   return (
     <div className="new-comment-container">
       <h3>Add a new Comment: </h3>
@@ -8,8 +24,12 @@ export const NewComment = () => {
         cols="30"
         rows="10"
         placeholder="Add a comment..."
+        value={text}
+        onChange={handleText}
       ></textarea>
-      <button>Add</button>
+      <button type="button" onClick={handleAddBtn}>
+        Add
+      </button>
     </div>
   );
 };
